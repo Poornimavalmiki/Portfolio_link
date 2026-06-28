416dd7b1f1064b5fb9332c2dc1c98dba3f5f427c
Ajitk9108188507 <ajitkakhandaki8@gmail.com>
2026-06-28 15:53:30 +0530

    Fixed GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<AboutPage/>}/>
