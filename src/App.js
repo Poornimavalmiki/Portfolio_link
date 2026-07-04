910b87b2f27a83dcad9922b9267b43c59ad614c8
Ajitk9108188507 <ajitkakhandaki8@gmail.com>
2026-07-04 08:55:43 +0530

    Upload portfolio

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-
-
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home';
-import AboutPage from './pages/AboutPage';
-import SkillsPage from './pages/SkillsPage';
-import ContactPage from './pages/ContactPage';
-import ExperiencePage from './pages/ExperiencePage';
-import EducationPage from './pages/EducationPage';
-import ProjectPage from './pages/ProjectPage';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage";
+import SkillsPage from "./pages/SkillsPage";
+import ContactPage from "./pages/ContactPage";
+import ExperiencePage from "./pages/ExperiencePage";
+import EducationPage from "./pages/EducationPage";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<AboutPage/>}/>
-          <Route path='/skills' element={<SkillsPage/>}/>
-          <Route path='/contact' element={<ContactPage/>}/>
-          <Route path='/experience' element={<ExperiencePage/>}/>
-          <Route path='/education' element={<EducationPage/>}/>
-          <Route path='/project' element={<ProjectPage/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/experience" element={<ExperiencePage />} />
+          <Route path="/education" element={<EducationPage />} />
+          <Route path="/project" element={<ProjectPage />} />
         </Routes>
-        
-        </BrowserRouter>
-       
-      </header>
+      </HashRouter>
     </div>
   );
 }
 
 export default App;
+
+// import {HashRouter,Routes,Route} from 'react-router-dom'
+// import Home from './pages/Home';
+// import AboutPage from './pages/AboutPage';
+// import SkillsPage from './pages/SkillsPage';
+// import ContactPage from './pages/ContactPage';
+// import ExperiencePage from './pages/ExperiencePage';
+// import EducationPage from './pages/EducationPage';
+// import ProjectPage from './pages/ProjectPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <HashRouter basename={process.env.PUBLIC_URL}>
+//         <Routes>
+//           <Route path='/' element={<Home/>}/>
+//           <Route path='/about' element={<AboutPage/>}/>
+//           <Route path='/skills' element={<SkillsPage/>}/>
+//           <Route path='/contact' element={<ContactPage/>}/>
+//           <Route path='/experience' element={<ExperiencePage/>}/>
+//           <Route path='/education' element={<EducationPage/>}/>
+//           <Route path='/project' element={<ProjectPage/>}/>
+//         </Routes>
+        
+//         </HashRouter>
+       
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
